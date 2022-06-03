205702b10ab6b022839a9967c5c34f3c4bf83d37
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2022-06-03 10:54:06 +0200

    adding superset package and login test cases

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,6 @@
 import { list } from 'mongoose-crudl'
 import AdminModel from '../models/Admin.js'
+import AuthenticationError from '../errors/AuthenticationError.js'
 import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
 
@@ -8,9 +9,11 @@ export default (apiServer) => {
 
   apiServer.post('/v1/login', async req => {
     req.body.email = req.body.email.toLowerCase()
-    const findUser = await list(AdminModel, { email: req.body.email }, req.query)
     const hash = crypto.createHash('md5').update(req.body.password).digest('hex')
-    if (findUser.result.password === hash) {
+    const findUser = await list(AdminModel, { email: req.body.email, password: hash }, req.query)
+    if(findUser.result.items.length === 0  ){
+      throw new AuthenticationError('Invalid email or password');
+    }
       const payload = {
         type: 'admin-login',
         user: {
@@ -25,6 +28,5 @@ export default (apiServer) => {
           loginToken: 'Bearer ' + token
         }
       }
-    }
   })
 }
--- src/routes/login.test.js
+++ src/routes/login.test.js
@@ -1,5 +1,85 @@
-describe('/v1/login/', () => {
-  test('true', () => {
-    expect(true).toBe(true)
+import mongoose from 'mongoose'
+import request from 'supertest'
+import crypto from 'crypto'
+import createMongooseMemoryServer from 'mongoose-memory'
+
+import createServer from './index.js'
+
+
+import AuthenticationError from '../errors/AuthenticationError.js'
+import Admin from '../models/Admin.js'
+
+const mongooseMemoryServer = createMongooseMemoryServer(mongoose)
+
+
+describe('/v1/login/ ', () => {
+  let app
+  beforeAll(async () => {
+    await mongooseMemoryServer.start()
+    await mongooseMemoryServer.connect('test-db')
+
+    app = createServer()
   })
+
+  afterEach(async () => {
+    await mongooseMemoryServer.purge()
+  })
+
+  afterAll(async () => {
+    await mongooseMemoryServer.disconnect()
+    await mongooseMemoryServer.stop()
+  })
+
+  test('login with email and password', async () => {
+
+      const hash1 = crypto.createHash('md5').update("user1Password").digest('hex')
+      const user1 = new Admin({ email: 'user1@gmail.com', password: hash1 })
+      await user1.save()
+
+      const hash2 = crypto.createHash('md5').update("user2Password").digest('hex')
+      const user2 = new Admin({ email: 'user2@gmail.com', password: hash2 })
+      await user2.save()
+
+      const res = await request(app)
+        .post('/v1/tasks/')
+        .send({email: user1.email, password: user1.password })
+        console.log(res.body);
+        expect(res).not.toThrow()
+      expect(res.body.status).toBe(200)
+    })
+
+
+      test('login with wronge email', async () => {
+
+          const hash1 = crypto.createHash('md5').update("user1Password").digest('hex')
+          const user1 = new Admin({ email: 'user1@gmail.com', password: hash1 })
+          await user1.save()
+
+          const res = await request(app)
+            .post('/v1/tasks/')
+            .send({email: "user3@gmail.com", password: user1.password })
+            console.log(res.body);
+            expect(res).not.toThrow()
+          expect(res.body.status).toBe(200)
+        })
+
+        test('login with wronge password', async () => {
+
+            const hash1 = crypto.createHash('md5').update("user1Password").digest('hex')
+            const user1 = new Admin({ email: 'user1@gmail.com', password: hash1 })
+            await user1.save()
+
+            const hash2 = crypto.createHash('md5').update("user2Password").digest('hex')
+            const user2 = new Admin({ email: 'user2@gmail.com', password: hash2 })
+            await user2.save()
+
+            const res = await request(app)
+              .post('/v1/tasks/')
+              .send({email: user1.email, password: user2.password })
+              console.log(res.body);
+              expect(res).not.toThrow()
+            expect(res.body.status).toBe(200)
+          })
+
+
 })
