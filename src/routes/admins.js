23aa04eab88b2c287ef5413b4c17fe859c0c00cc
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-06-09 13:19:09 +0200

    add aws configs and postBinary route

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -6,15 +6,20 @@ import crypto from 'crypto'
 
 import jwt from 'jsonwebtoken'
 import handlebars from 'handlebars'
+import mime from 'mime-types'
 
 import { list, readOne, deleteOne, patchOne } from 'mongoose-crudl'
 import { AuthorizationError, MethodNotAllowedError, ValidationError, AuthenticationError } from 'standard-api-errors'
 import allowAccessTo from 'bearer-jwt-auth'
 
 import AdminModel from '../models/Admin.js'
+import aws from '../helpers/awsBucket.js'
 import sendEmail from 'aws-ses-send-email'
 
 const secrets = process.env.SECRETS.split(' ')
+const baseUrl = process.env.STATIC_SERVER_URL
+const bucketName = process.env.AWS_BUCKET_NAME
+const s3 = await aws()
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const VerifyEmail = fs.readFileSync(path.join(__dirname, '..', 'email-templates', 'verifyEmail.html'), 'utf8')
@@ -149,6 +154,23 @@ export default (apiServer) => {
   apiServer.patch('/v1/admins/:id/email-confirm', async req => {
     const data = await allowAccessTo(req, secrets, [{ type: 'verfiy-email', user: { _id: req.params.id } }])
     await patchOne(AdminModel, { id: req.params.id }, { email: data.newEmail })
+    return {
+      status: 200,
+      result: {
+        success: true
+      }
+    }
+  })
+
+  apiServer.postBinary('/v1/admins/:id/upload-avatar', { mimeTypes: ['image/jpeg', 'image/png', 'image/gif'], fieldName: 'avatar' }, async req => {
+    allowAccessTo(req, secrets, [{ type: 'admin', user: { _id: req.params.id } }])
+    const uploadParams = {
+      Bucket: bucketName,
+      Body: req.file.buffer,
+      Key: `mua-system-admins/${req.params.id}.${mime.extension(req.file.mimetype)}`
+    }
+    const result = await s3.upload(uploadParams).promise()
+    await patchOne(AdminModel, { id: req.params.id }, { avatar: baseUrl + result.Key })
     return {
       status: 200,
       result: {
