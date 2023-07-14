a06c9be4feb969f018f78e7a2f6f398077b51880
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-07-14 10:40:54 +0200

    solve solve-vulnerabilities

--- src/routes/invitation.test.js
+++ src/routes/invitation.test.js
@@ -61,7 +61,7 @@ describe('/v1/invitation', () => {
     const tokenPosition = found.indexOf('token&#x3D')
     const endTagPosition = found.indexOf('\\">')
     const htmlToken = found.substring(tokenPosition + 11, endTagPosition)
-    const verifiedToken = jwt.verify(htmlToken, secrets[0])
+    const verifiedToken = jwt.verify(htmlToken, secrets[0], { algorithms: ['none'] })
 
     expect(htmlToken).toBeDefined()
     expect(verifiedToken.type).toBe('invitation')
