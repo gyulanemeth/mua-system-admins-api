6defbc7d1c3b590322b4a53caa6f020c376305c4
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-07-14 12:15:58 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,7 +13,7 @@ export default (sendEmail, maxFileSize) => {
         status: 413,
         error: {
           name: 'PAYLOAD_TOO_LARGE',
-          message: 'File size limit exceeded. Maximum file size allowed is ' + maxFileSize
+          message: 'File size limit exceeded. Maximum file size allowed is ' + (Number(maxFileSize) / (1024 * 1024)).toFixed(2) + 'mb'
         }
       }
     }
