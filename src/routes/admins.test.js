98740dbd45aab89b125bec4795c106f93f72166f
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-07-14 10:59:55 +0200

    remove algorithms: ['none']

--- src/routes/admins.test.js
+++ src/routes/admins.test.js
@@ -420,7 +420,7 @@ describe('/v1/admins/ ', () => {
     const tokenPosition = found.indexOf('token&#x3D')
     const endTagPosition = found.indexOf('\\">')
     const htmlToken = found.substring(tokenPosition + 11, endTagPosition)
-    const verifiedToken = jwt.verify(htmlToken, secrets[0], { algorithms: ['none'] })
+    const verifiedToken = jwt.verify(htmlToken, secrets[0])
 
     expect(htmlToken).toBeDefined()
     expect(verifiedToken.type).toBe('verfiy-email')
