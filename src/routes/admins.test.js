b634d5370d2a3ad102ddd3c85feebf86b090c963
Ahmed Qudaih <54820415+AhmedQudaih@users.noreply.github.com>
2023-07-14 10:24:09 +0200

    solve-vulnerabilities

--- src/routes/admins.test.js
+++ src/routes/admins.test.js
@@ -420,7 +420,7 @@ describe('/v1/admins/ ', () => {
     const tokenPosition = found.indexOf('token&#x3D')
     const endTagPosition = found.indexOf('\\">')
     const htmlToken = found.substring(tokenPosition + 11, endTagPosition)
-    const verifiedToken = jwt.verify(htmlToken, secrets[0])
+    const verifiedToken = jwt.verify(htmlToken, secrets[0], { algorithms: ['none'] })
 
     expect(htmlToken).toBeDefined()
     expect(verifiedToken.type).toBe('verfiy-email')
--- src/routes/forgotPassword.test.js
+++ src/routes/forgotPassword.test.js
@@ -59,7 +59,7 @@ describe('/v1/forgot-password/', () => {
     const tokenPosition = found.indexOf('token&#x3D')
     const endTagPosition = found.indexOf('\\">')
     const htmlToken = found.substring(tokenPosition + 11, endTagPosition)
-    const verifiedToken = jwt.verify(htmlToken, secrets[0])
+    const verifiedToken = jwt.verify(htmlToken, secrets[0], { algorithms: ['none'] })
     expect(htmlToken).toBeDefined()
     expect(verifiedToken.type).toBe('forgot-password')
     expect(verifiedToken.user.email).toBe(user2.email)
