ab59c41b42ff3d7ac267330a5d9d7b075cd17c88
AhmedQudaih <ahmedku0123@gmail.com>
2024-05-23 11:55:47 +0300

    remove config and modles

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import admins from './routes/admins.js'
 import login from './routes/login.js'
 import invitation from './routes/invitation.js'
 import forgotPassword from './routes/forgotPassword.js'
-import config from './routes/config.js'
 
 export {
   admins,
   login,
   invitation,
-  forgotPassword,
-  config
+  forgotPassword
 }
